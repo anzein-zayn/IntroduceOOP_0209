d4910cbf87e597cf2facaacb637608f47b9c9700
anzein-zayn <andy.zaky.ft24@mail.umy.ac.id>
2025-04-28 19:11:05 +0700

    Membuat  member kharakter dan memberi akses

--- IntroduceOOP.cpp
+++ IntroduceOOP.cpp
@@ -7,24 +7,3 @@ class Planet{
     public:
         string Nama;
         float Diameter;
- void displayData(){
-    cout << ", Gravitasiku" << Gravitasi<<endl;
- }
-void inputData(){
-    cout << "Input Nama : ";
-    cin >> Nama;
-    cout << "Input Ukuran : ";
-    cin >> Diameter;
-    cout << "Input Gravitasi : ";
-    cin >> Gravitasi;
-}
-};
-
-int main()
-{
-    Planet plnt;
-    plnt.inputData();
-    cout << "Nama-nya adalah "<< plnt.Nama<< endl;
-    cout << "Diameter-nya adalah " << plnt.Diameter;
-    plnt.displayData();
-}
